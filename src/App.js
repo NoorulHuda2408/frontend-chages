ddc0d724fa9a2d2b5bc252ba1c80fd5622031410
haris <m.haris1694@gmail.com>
2023-08-24 19:25:56 +0500

    routing changes

--- src/App.js
+++ src/App.js
@@ -1,40 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
-import Navbar from "./pages/Navbar"
-import HeroSection from "./pages/HeroSection.js"
 import MainloginPage from "./pages/mainLoginPage/MainloginPage"; 
-import Success from './components/Success/Success'
-import TOTC from './components/TOTC';
-import TeacherTools from "./components/TeacherTools";
-// import Assesments from "./components/Assesment";
-import Assesments from "./components/Assesment/Assesment";
-import One from "./components/One/One"
-import ClassManagement from './components/ClassManagement/ClassManagement';
-import Cloud from "./components/Cloud/Cloud"
-import Footer from "./components/Footer/Footer"
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Dashboard from './pages/Dashboard/Dashboard';
 function App() {
   return (
     <div className="App">
-     {/* <MainloginPage/> */}
-     {/* <Navbar/>
-     <HeroSection/>
-     <Success/>
-     <Cloud/>
-     <TOTC/>
-     <TeacherTools/>
-     <Assesments/>
-     <ClassManagement/>
-     <One/>
-     <Footer/> */}
-     
+  
      <Router>
     <Routes>
         <Route exact path="/" element={<MainloginPage />} />
          <Route exact path="/dashboard" element={<Dashboard />} />
-    
     </Routes>
 </Router>
 
